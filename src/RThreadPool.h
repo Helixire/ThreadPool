1050310923b75a5b38e0f0cc5ae65d052a6c2c58
Pierre <pierre.blank@lumithd.fr>
2017-06-19 15:43:36 +0200

    init folder and cmake

---
+++ src/RThreadPool.h
@@ -0,0 +1,17 @@
+#ifndef __RTHREADPOOL_H__
+#define __RTHREADPOOL_H__
+
+#include "RThread.h"
+
+namespace RPTR
+{
+    class ThreadPool
+    {
+    public:
+        //Number of thread in the thread pool
+        ThreadPool(const unsigned int nb);
+        
+    };
+}
+
+#endif /* __RTHREADPOOL_H__ */
