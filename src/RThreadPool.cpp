55955c29016a717682dd4df1c931b27ecf850256
Pierre <pierre.blank@lumithd.fr>
2017-06-19 17:30:32 +0200

    ThreadPool v1

--- src/RThreadPool.cpp
+++ src/RThreadPool.cpp
@@ -1,1 +1,49 @@
 #include "RThreadPool.h"
+
+RPTR::ThreadPool::ThreadPool(const unsigned int nb) : m_nb(nb)
+{
+    m_data.running = true;
+    init(nb);
+}
+
+RPTR::ThreadPool::~ThreadPool()
+{
+    m_data.running = false;
+    m_data.sem.add(m_nb);
+}
+
+void RPTR::ThreadPool::add_task(void (*funct)(void *), void *data)
+{
+    m_data.mut.lock();
+    m_data.list.push({funct, data});
+    m_data.mut.unlock();
+    m_data.sem.post();
+}
+
+void RPTR::ThreadPool::init(unsigned int nb)
+{
+    if (!nb)
+        nb = 4; //TODO auto find correct number of thread
+    m_nb = nb;
+    m_pool = new Thread[nb];
+    for (; nb; --nb)
+        m_pool[nb].start((void (*)(void *))thread_main, &m_data);
+}
+
+
+void RPTR::ThreadPool::thread_main(in_data *data)
+{
+    cmd tmp;
+    
+    while (data->running)
+    {
+        data->sem.wait();
+        if (!data->running)
+            break;
+        data->mut.lock();
+        tmp = data->list.front();
+        data->list.pop();
+        data->mut.unlock();
+        tmp.funct(tmp.data);
+    }
+}
--- src/RThreadPool.h
+++ src/RThreadPool.h
@@ -1,7 +1,10 @@
 #ifndef __RTHREADPOOL_H__
 #define __RTHREADPOOL_H__
 
+#include <queue>
 #include "RThread.h"
+#include "RSemaphore.h"
+#include "RMutex.h"
 
 namespace RPTR
 {
@@ -9,8 +12,30 @@ namespace RPTR
     {
     public:
         //Number of thread in the thread pool
-        ThreadPool(const unsigned int nb);
+        ThreadPool(const unsigned int nb = 0);
+        ~ThreadPool();
+
+        void    add_task(void (*funct)(void *), void *data);
+    private:
+        struct cmd
+        {
+            void    (*funct)(void*);
+            void    *data;
+        };
+        struct in_data
+        {
+            Semaphore       sem;
+            Mutex           mut;
+            std::queue<cmd> list;
+            bool            running;
+        };
         
+        void init(unsigned int nb);
+        static void thread_main(in_data *);
+        
+        in_data         m_data;
+        Thread          *m_pool;
+        unsigned int    m_nb;
     };
 }
 
